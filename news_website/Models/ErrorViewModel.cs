bba84bf5a6ea00b8e0cf8bb0a3a26bb32c0c583b
unknown <samiratousalifou@gmail.com>
2025-07-12 06:37:49 +0100

    first commit

---
+++ news_website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace news_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
